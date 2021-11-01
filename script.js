c7b94ef52a46cf5d90f37c2afb1f75bac5475d13
Eoin Harkin <eharky@gmail.com>
2021-11-01 09:56:20 +0000

    changed html values and added function to make button return our password

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
-var smallLetters  = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+//var generateBtn = document.querySelector("#generate");
+
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+//function writePassword() {
+  //var password = generatePassword();
+  //var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  //passwordText.value = password;
 
-}
+//}
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword)
+//generateBtn.addEventListener("click", writePassword)
 
-////////// my code ///////////////////
+///////////////////////////////////////comented out default code as I would like to build from scratch and feel it is making it more complicated
+
+
+
 //Fisher-yates randomize function
 String.prototype.shuffle = function () {
   var a = this.split(""),
@@ -29,6 +31,9 @@ String.prototype.shuffle = function () {
   return a.join("");
 }
 
+////////// my code ///////////////////
+
+
 var numberArray = ["1","2","3","4","5","6","7","8","9","10"]
 var smallLetters  = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var bigLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
@@ -40,41 +45,56 @@ var bigLettersValue = true
 var symbolsValue = true
 
 // empty array
-var password = []
+var passwordArray = []
 
 
 
 var v = 1
-var userLimit = prompt("enter how many items you would like in this loop")
+//switched off prompt to stop alert while editing css and  html    prompt("enter how many items you would like in this loop")
+var userLimit = 25
 
-for (let v = 0; v < userLimit ; v++) {
-  if (numberArrayValue == true) {
-    var c = numberArray[Math.floor(Math.random()*numberArray.length)]
-        password.push(c);
-        
-    }
+var generatePassword = function() {
 
-  if (smallLettersValue == true) {
-    var x = smallLetters[Math.floor(Math.random()*smallLetters.length)]
-        password.push(x);
-        
-    }
-  if (bigLettersValue == true) {
-    var z = bigLetters[Math.floor(Math.random()*bigLetters.length)]
-        password.push(z);
-        
-    }
-    if (symbolsValue == true) {
+  for (let v = 0; v < userLimit ; v++) {
+      if (numberArrayValue == true) {
+          var c = numberArray[Math.floor(Math.random()*numberArray.length)]
+          passwordArray.push(c);
+          console.log(passwordArray)
+              
+          }
+
+      if (smallLettersValue == true) {
+          var x = smallLetters[Math.floor(Math.random()*smallLetters.length)]
+          passwordArray.push(x);
+          console.log(passwordArray)
+              
+          }
+      if (bigLettersValue == true) {
+          var z = bigLetters[Math.floor(Math.random()*bigLetters.length)]
+          passwordArray.push(z);
+          console.log(passwordArray)
+              
+          }
+      if (symbolsValue == true) {
       var y = symbols[Math.floor(Math.random()*symbols.length)]
-          password.push(y);
+          passwordArray.push(y);
+          console.log(passwordArray)
           
       }
+      var joinedPassword = passwordArray.join("")
+      console.log(joinedPassword)
 
+      var slicedPassword = joinedPassword.slice(0,userLimit)
+      console.log(slicedPassword)
+
+      var shuffledPassword = slicedPassword.shuffle()
+      console.log(shuffledPassword)
+      }
 }
 
-console.log(password)
+console.log(passwordArray)
 
-var joinedPassword = password.join("")
+var joinedPassword = passwordArray.join("")
 console.log(joinedPassword)
 
 var slicedPassword = joinedPassword.slice(0,userLimit)
@@ -89,10 +109,7 @@ console.log(shuffledPassword)
 // done
 
 
-
-
-
-
+//working as of sunday night need to figure out how to connect back and front end 
 
 
 
